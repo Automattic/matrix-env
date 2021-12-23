62dffb402c91d7e84503fe03db4dc0b8150356e2
Ashfame <mail@ashfame.com>
2021-12-23 19:41:07 +0400

    utilize postgres default user itself and do not bother with creating the user & granting it privileges

--- postgres/run.sh
+++ postgres/run.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER slackbridge_user WITH PASSWORD 'slackbridgesecret';
+psql -v ON_ERROR_STOP=1 <<-EOSQL
     CREATE DATABASE slack_bridge;
-    GRANT ALL PRIVILEGES ON DATABASE slack_bridge to slackbridge_user;
 EOSQL
